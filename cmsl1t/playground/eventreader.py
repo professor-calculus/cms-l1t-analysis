57b80c9aadc7c604670c219672a5be9de3368652
kreczko <lkreczko@googlemail.com>
2017-02-22 15:48:23 +0000

    added jet matching

--- cmsl1t/playground/eventreader.py
+++ cmsl1t/playground/eventreader.py
@@ -3,8 +3,10 @@ from rootpy.tree import Tree, TreeChain
 #from six.moves import zip
 import six
 import inspect
+import math
 
 from jetfilters import defaultJetFilter
+
 
 class Event(object):
 
@@ -42,7 +44,7 @@ class Event(object):
         # for m in members:
         #     print('>' * 6, m[0], ':', m[1])
 
-    def goodJets(self, jetFilter = defaultJetFilter):
+    def goodJets(self, jetFilter=defaultJetFilter):
         '''
             filters and ET orders the jet collection
         '''
@@ -51,19 +53,40 @@ class Event(object):
             goodJets, key=lambda jet: jet.etCorr, reverse=True)
         return sorted_jets
 
-    def getLeadingRecoJet(self, jetFilter = defaultJetFilter):
+    def getLeadingRecoJet(self, jetFilter=defaultJetFilter):
         goodJets = self.goodJets(jetFilter)
+        if not goodJets:
+            return None
         leadingRecoJet = goodJets[0]
         if leadingRecoJet.etCorr > 10.0:
             return leadingRecoJet
         return None
 
-    def getMatchedL1Jet(self, l1Type):
+    def getMatchedL1Jet(self, recoJet, l1Type='HW'):
         l1Jets = None
         if l1Type == 'HW':
-            l1Jets = self._upgrade.L1Upgrade
+            l1Jets = [L1Jet(self._upgrade.L1Upgrade, i)
+                      for i in range(self._upgrade.L1Upgrade.nJets)]
         if l1Type == 'EMU':
-            l1Jets = self._emuUpgrade.L1Upgrade
+            l1Jets = [L1Jet(self._emuUpgrade.L1Upgrade, i)
+                      for i in range(self._emuUpgrade.L1Upgrade.nJets)]
+
+        if not l1Jets or not recoJet:
+            return None
+        minDeltaR = 0.3
+        closestJet = None
+        for l1Jet in l1Jets:
+            dEta = recoJet.eta - l1Jet.eta
+            dPhi = recoJet.phi - l1Jet.phi
+            dR = math.sqrt(dEta**2 + dPhi**2)
+            if dR < minDeltaR:
+                minDeltaR = dR
+                closestJet = l1Jet
+        return closestJet
+
+    @property
+    def nVertex(self):
+        return self._recoTree.Vertex.nVtx
 
 
 class Jet(object):
@@ -84,10 +107,20 @@ class Jet(object):
     @property
     def sumMult(self):
         return self.chMult + self.chMult + self.elMult
+        # not
+        # return self.chMult + self.muMult + self.elMult ?
 
     @property
     def allMult(self):
         return self.sumMult + self.nhMult + self.phMult
+
+
+class L1Jet(object):
+
+    def __init__(self, l1Jets, index):
+        self.et = l1Jets.jetEt[index]
+        self.eta = l1Jets.jetEta[index]
+        self.phi = l1Jets.jetPhi[index]
 
 
 class EventReader(object):
