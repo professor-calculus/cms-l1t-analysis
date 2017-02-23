b93c4abbab7e9bfbc44fe33c6b62b7b3db1909d7
kreczko <lkreczko@googlemail.com>
2017-02-23 16:56:14 +0000

    simplified jetFilter

--- cmsl1t/playground/jetfilters.py
+++ cmsl1t/playground/jetfilters.py
@@ -1,29 +1,27 @@
+import cmsl1t.geometry as geo
+
 
 def jetFilterNoHF(jet):
     '''
         also a lot of potential for simplification
     '''
-    if jet.muMult != 0:
-        return False
-    if abs(jet.eta) > 3.0:
-        return False
-    if jet.nhef >= 0.9:
-        return False
-    if jet.pef >= 0.9:
-        return False
-    if jet.mef >= 0.8:
-        return False
-    if jet.allMult <= 1:
-        return False
-    if abs(jet.eta) > 2.4:
-        return True
-    if jet.sumMult <= 0:
-        return False
-    if jet.chef <= 0:
-        return False
-    if jet.eef >= 0.9:
+    isForwardJet = geo.isInRegion('HF', jet.eta)
+    innerJet = abs(jet.eta) <= 2.4
+    reject_if = [
+        jet.muMult != 0,
+        isForwardJet,
+        jet.nhef >= 0.9,
+        jet.pef >= 0.9,
+        jet.mef >= 0.8,
+        jet.allMult <= 1,
+        innerJet and jet.sumMult <= 0,
+        innerJet and jet.chef <= 0,
+        innerJet and jet.eef >= 0.9,
+    ]
+    if any(reject_if):
         return False
     return True
+
 
 def defaultJetFilter(jet):
     return (abs(jet.eta) > 3.0 or jetFilterNoHF(jet)) and jet.etCorr > 30.
