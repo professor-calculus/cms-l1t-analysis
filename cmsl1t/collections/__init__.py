954e97cdfb945193c500cf50562dd646563d8e4c
kreczko <lkreczko@googlemail.com>
2017-03-07 11:09:37 +0000

    added first draft for EfficiencyCollection

--- cmsl1t/collections/__init__.py
+++ cmsl1t/collections/__init__.py
@@ -4,9 +4,11 @@ from __future__ import absolute_import
 from .base import BaseHistCollection
 from .by_pileup import HistogramsByPileUpCollection
 from .resolution import ResolutionCollection
+from .efficiency import EfficiencyCollection
 
 __all__ = [
     'BaseHistCollection',
     'HistogramsByPileUpCollection',
     'ResolutionCollection',
+    'EfficiencyCollection',
 ]
