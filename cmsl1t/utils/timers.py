18d76cb81c9181c3fc573abd87e71fe5ce73ac08
Ben Krikler <b.krikler@cern.ch>
2017-03-29 09:20:14 +0200

    Move timerfunc from run_benchmark into a util file so it can be used elswhere

---
+++ cmsl1t/utils/timers.py
@@ -0,0 +1,23 @@
+from __future__ import print_function
+import time
+
+def timerfunc(printer=print):
+    """
+    A timer decorator
+    """
+    def wrapper(func):
+        def function_timer(*args, **kwargs):
+            """
+            A nested function for timing other functions
+            """
+            start = time.time()
+            value = func(*args, **kwargs)
+            end = time.time()
+            runtime = end - start
+            msg = "The runtime for {func} took {time} seconds to complete"
+            printer(msg.format(func=func.__name__,
+                             time=runtime))
+            return value
+        return function_timer
+    return wrapper
+
