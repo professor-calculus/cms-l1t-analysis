14888c81a121f9ac2c0a5509ab2cbd84191e86a3
Ben Krikler <b.krikler@cern.ch>
2017-05-21 01:50:45 +0200

    Fix Pep8 formatting

--- cmsl1t/collections/hist.py
+++ cmsl1t/collections/hist.py
@@ -4,8 +4,8 @@ from exceptions import RuntimeError, KeyError, NotImplementedError
 
 
 class DimensionBase():
-    overflow="overflow"
-    underflow="underflow"
+    overflow = "overflow"
+    underflow = "underflow"
 
     def __len__(self):
         return self.n_bins
@@ -13,28 +13,29 @@ class DimensionBase():
 
 class DimensionSorted(DimensionBase):
     import bisect
-    def __init__(self,bin_edges):
+
+    def __init__(self, bin_edges):
         self.bins = sorted(bin_edges)
         self.n_bins = len(self.bins)
 
-    def __getitem__(self,value):
+    def __getitem__(self, value):
         if value < self.bins[0]:
             found_bin = self.underflow
         elif value >= self.bins[-1]:
             found_bin = self.overflow
         else:
-            found_bin = bisect.bisect(self.bins,value) - 1
+            found_bin = bisect.bisect(self.bins, value) - 1
         return [found_bin]
 
 
 class DimensionOverlappingBins(DimensionBase):
-    def __init__(self,bins):
+    def __init__(self, bins):
         self.bins = bins
         self.n_bins = len(self.bins)
 
-    def __getitem__(self,value):
+    def __getitem__(self, value):
         contained_in = []
-        for i,(bin_low,bin_high) in enumerate(self.bins):
+        for i, (bin_low, bin_high) in enumerate(self.bins):
             if value >= bin_low and value < bin_high:
                 contained_in.append(i)
         if len(contained_in) == 0:
@@ -44,13 +45,14 @@ class DimensionOverlappingBins(DimensionBase):
 
 class DimensionRegion(DimensionBase):
     from cmsl1t.geometry import eta_regions
+
     def __init__(self):
         self.n_bins = len(self.eta_regions)
 
-    def __getitem__(self,value):
+    def __getitem__(self, value):
         regions = []
         for region, is_contained in self.eta_regions.iteritems():
-            if is_contained(value): 
+            if is_contained(value):
                 regions.append(region)
         return regions
 
@@ -62,20 +64,20 @@ class HistogramCollection(object):
      - needs to know how to create new histograms
     '''
 
-    def __init__(self, dimensions,histogram_factory):
+    def __init__(self, dimensions, histogram_factory):
         '''
             Should dimensions include or exclude histogram names?
         '''
-        if not isinstance(dimensions,list):
-            dimensions=[dimensions]
+        if not isinstance(dimensions, list):
+            dimensions = [dimensions]
         for dim in dimensions:
-            if not isinstance(dim,DimensionBase):
+            if not isinstance(dim, DimensionBase):
                 raise RuntimeError("non-Dimension object given to histogram")
         self._dimensions = dimensions
         self._hists = collections.defaultdict(histogram_factory)
 
     @classmethod
-    def _flatten_bins(self,bins):
+    def _flatten_bins(self, bins):
         flattened_bins = []
         for dimension in bins:
             if len(flattened_bins) == 0:
@@ -84,30 +86,30 @@ class HistogramCollection(object):
             else:
                 new_bins = []
                 for previous in flattened_bins:
-                    new_bins += [ previous+[index] for index in dimension ]
+                    new_bins += [previous+[index] for index in dimension]
                 flattened_bins = new_bins
         output_bin_list = []
         for bin in flattened_bins:
             output_bin_list.append(tuple(bin))
         return output_bin_list
 
-    def _find_bins(self,keys):
+    def _find_bins(self, keys):
         # In python 3.3, this becomes collections.abc.Sequence
-        if not isinstance(keys, collections.Sequence) :
-            if len(self._dimensions)>1:
-                msg="Single key given when "+len(self._dimensions)+" needed"
+        if not isinstance(keys, collections.Sequence):
+            if len(self._dimensions) > 1:
+                msg = "Single key given when "+len(self._dimensions)+" needed"
                 raise KeyError(msg)
-            keys=[keys]
+            keys = [keys]
         elif len(self._dimensions) != len(keys):
-            msg="Number of keys does not match no. of dimensions\n"
-            msg+="Given {0}, needed {1}".format(
-                    len(keys), len(seef._dimeesions))
+            msg = "Number of keys does not match no. of dimensions\n"
+            msg += "Given {0}, needed {1}".format(
+                    len(keys), len(self._dimeesions))
             raise KeyError(msg)
 
         # Check every dimension if it contains these values
         bins = []
         for key, dimension in zip(keys, self._dimensions):
-             bins.append(dimension[key])
+            bins.append(dimension[key])
 
         # Some dimensions might return multiple values, flatten returned arrays
         bins = self._flatten_bins(bins)
@@ -123,12 +125,12 @@ class HistogramCollection(object):
         '''
         hist_indices = self._find_bins(keys)
         if len(hist_indices) > 1:
-             msg="""HistogramCollection.__getitem__ not fully implemented for
-                    dimensions with overlapping bins"""
-             raise NotImplementedError(msg)
+            msg = """HistogramCollection.__getitem__ not fully implemented for
+                   dimensions with overlapping bins"""
+            raise NotImplementedError(msg)
         return self._hists[hist_indices[0]]
 
-    def __setitem__(self, keys,value):
+    def __setitem__(self, keys, value):
         '''
             Supposed to handle
                 coll[x]
@@ -140,7 +142,7 @@ class HistogramCollection(object):
             self._hists[indices] = value
 
     def shape(self):
-        _shape = [ len(dim) for dim in self._dimensions ]
+        _shape = [len(dim) for dim in self._dimensions]
         return tuple(_shape)
 
     def __len__(self):
--- test/collections/test_histogramCollection.py
+++ test/collections/test_histogramCollection.py
@@ -1,44 +1,45 @@
 from __future__ import print_function
 from nose.tools import assert_equal
 import cmsl1t.collections.hist as hist
-from cmsl1t.geometry import is_in_region, eta_regions
-from functools import partial
 
 
 pileup = hist.DimensionSorted([0, 10, 15, 20, 30, 999])
-multi = hist.DimensionOverlappingBins([(0,10),(100,110),(5,15)])
+multi = hist.DimensionOverlappingBins([(0, 10), (100, 110), (5, 15)])
 regions = hist.DimensionRegion()
-dummy_factory = lambda: print("making histogram")
+
+
+def dummy_factory():
+    print("making histogram")
 
 
 def test_dimension_sorted():
-    assert_equal(pileup[-19],[hist.DimensionBase.underflow])
-    assert_equal(pileup[9],[0])
-    assert_equal(pileup[19],[2])
-    assert_equal(pileup[39],[4])
-    assert_equal(pileup[9999],[hist.DimensionBase.overflow])
+    assert_equal(pileup[-19], [hist.DimensionBase.underflow])
+    assert_equal(pileup[9], [0])
+    assert_equal(pileup[19], [2])
+    assert_equal(pileup[39], [4])
+    assert_equal(pileup[9999], [hist.DimensionBase.overflow])
 
 
 def test_dimension_multi():
-    assert_equal(multi[3],[0])
-    assert_equal(multi[7],[0,2])
-    assert_equal(multi[105],[1])
-    assert_equal(multi[70],[hist.DimensionBase.overflow])
+    assert_equal(multi[3], [0])
+    assert_equal(multi[7], [0, 2])
+    assert_equal(multi[105], [1])
+    assert_equal(multi[70], [hist.DimensionBase.overflow])
 
 
 def test_dimension_region():
-    assert_equal(regions[0],["BE", "B"])
-    assert_equal(regions[2],["BE", "E"])
-    assert_equal(regions[3.1],["HF"])
+    assert_equal(regions[0], ["BE", "B"])
+    assert_equal(regions[2], ["BE", "E"])
+    assert_equal(regions[3.1], ["HF"])
 
 
 def test_flatten_bin_list():
-    bin_list = [ [1] ]
+    bin_list = [[1]]
     flat_list = hist.HistogramCollection._flatten_bins(bin_list)
-    assert_equal(flat_list, [ (1,) ] )
-    bin_list = [ [1],[2,3] ]
+    assert_equal(flat_list, [(1,)])
+    bin_list = [[1], [2, 3]]
     flat_list = hist.HistogramCollection._flatten_bins(bin_list)
-    assert_equal(flat_list, [ (1,2),(1,3) ] )
+    assert_equal(flat_list, [(1, 2), (1, 3)])
 
 
 def test_find_bins():
@@ -48,10 +49,10 @@ def test_find_bins():
             )
     coll[11] = 2
     coll[42] = 42
-    assert_equal(coll._find_bins(-20), [ (hist.DimensionBase.underflow,)] )
-    assert_equal(coll._find_bins(13), [ (1,)] )
-    assert_equal(coll._find_bins(47), [ (4,)] )
-    assert_equal(coll._find_bins(9999), [ (hist.DimensionBase.overflow,)] )
+    assert_equal(coll._find_bins(-20), [(hist.DimensionBase.underflow, )])
+    assert_equal(coll._find_bins(13), [(1, )])
+    assert_equal(coll._find_bins(47), [(4, )])
+    assert_equal(coll._find_bins(9999), [(hist.DimensionBase.overflow, )])
 
 
 def test_pileup_binning():
@@ -64,17 +65,17 @@ def test_pileup_binning():
     coll[42] = 42
     coll[10044] = 49
     assert_equal(coll[-20], 6)
-    assert_equal(coll[13], 2 )
-    assert_equal(coll[47], 42 )
-    assert_equal(coll[9999], 49 )
+    assert_equal(coll[13], 2)
+    assert_equal(coll[47], 42)
+    assert_equal(coll[9999], 49)
 
 
 def test_collection_2D():
     coll = hist.HistogramCollection(
-            dimensions=[pileup,multi],
+            dimensions=[pileup, multi],
             histogram_factory=dummy_factory
             )
-    coll[-3,4] = 6
-    coll[11,105] = 49
-    assert_equal(coll[-20,2], 6)
-    assert_equal(coll[13,108], 49 )
+    coll[-3, 4] = 6
+    coll[11, 105] = 49
+    assert_equal(coll[-20, 2], 6)
+    assert_equal(coll[13, 108], 49)
