ef9493bccc1627b838787849dedf0238cc973d92
Ben Krikler <b.krikler@cern.ch>
2017-05-20 11:16:17 +0200

    Fix and extend first unit tests

--- test/collections/test_histogramCollection.py
+++ test/collections/test_histogramCollection.py
@@ -1,21 +1,54 @@
+from __future__ import print_function
 from nose.tools import assert_equal
 import cmsl1t.collections.hist as hist
 from cmsl1t.geometry import is_in_region, eta_regions
 from functools import partial
 
 
-pileupBins = [0, 10, 15, 20, 30, 999]
-regions = eta_regions.keys()
+pileup = hist.DimensionSorted([0, 10, 15, 20, 30, 999])
+dummy_factory = lambda: print("making histogram")
+
+
+def test_dimension_sorted():
+    assert_equal(pileup[-19],[hist.DimensionBase.underflow])
+    assert_equal(pileup[9],[0])
+    assert_equal(pileup[19],[2])
+    assert_equal(pileup[39],[4])
+    assert_equal(pileup[9999],[hist.DimensionBase.overflow])
+
+
+def test_flatten_bin_list():
+    bin_list = [ [1] ]
+    flat_list = hist.HistogramCollection._flatten_bins(bin_list)
+    assert_equal(flat_list, [ (1,) ] )
+
+
+def test_find_bins():
+    coll = hist.HistogramCollection(
+            dimensions=[pileup],
+            histogram_factory=dummy_factory
+            )
+    coll[11] = 2
+    coll[42] = 42
+    assert_equal(coll._find_bins(-20), [ (hist.DimensionBase.underflow,)] )
+    assert_equal(coll._find_bins(13), [ (1,)] )
+    assert_equal(coll._find_bins(47), [ (4,)] )
+    assert_equal(coll._find_bins(9999), [ (hist.DimensionBase.overflow,)] )
 
 
 def test_pileup_binning():
-    coll = hist.HistogramCollection(dimensions=1)
-    coll.register_dim(1, segmentation_func=partial(
-        hist.bin_finder_sorted, bins=pileupBins), bins=pileupBins)
+    coll = hist.HistogramCollection(
+            dimensions=[pileup],
+            histogram_factory=dummy_factory
+            )
+    coll[-3] = 6
     coll[11] = 2
     coll[42] = 42
-    assert_equal(coll[13], 2)
-    assert_equal(coll[9999], 42)
+    coll[10044] = 49
+    assert_equal(coll[-20], 6)
+    assert_equal(coll[13], 2 )
+    assert_equal(coll[47], 42 )
+    assert_equal(coll[9999], 49 )
 
 
 def test_region_binning():
