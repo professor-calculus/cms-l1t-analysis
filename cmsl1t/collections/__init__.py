898ef10757a3e3aa037d2adf1af1cc8397f6075b
Ben Krikler <b.krikler@cern.ch>
2017-05-23 12:10:25 +0200

    Remove HistCollection from __init__

--- cmsl1t/collections/__init__.py
+++ cmsl1t/collections/__init__.py
@@ -5,12 +5,10 @@ from .base import BaseHistCollection
 from .by_pileup import HistogramsByPileUpCollection
 from .resolution import ResolutionCollection
 from .efficiency import EfficiencyCollection
-from .hist import HistogramCollection
 
 __all__ = [
     'BaseHistCollection',
     'HistogramsByPileUpCollection',
     'ResolutionCollection',
     'EfficiencyCollection',
-    'HistogramCollection',
 ]
