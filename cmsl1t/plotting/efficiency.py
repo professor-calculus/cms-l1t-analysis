cdd09e62ef347ae4890aa6a9faacfde369a9c9e1
Ben Krikler <b.krikler@cern.ch>
2017-07-02 21:43:22 +0200

    Make use of new code to fit the turnons

--- cmsl1t/plotting/efficiency.py
+++ cmsl1t/plotting/efficiency.py
@@ -3,6 +3,7 @@ from cmsl1t.hist.hist_collection import HistogramCollection
 from cmsl1t.hist.factory import HistFactory
 import cmsl1t.hist.binning as bn
 from cmsl1t.utils.draw import draw, label_canvas
+from cmsl1t.utils.fit_turnon import fit_turnon
 from cmsl1t.io import to_root
 
 from rootpy.plotting import Legend, HistStack
@@ -55,35 +56,43 @@ class EfficiencyPlot():
 
     def draw(self, with_fits=True):
         # Calclate the efficiency for each threshold
-        self.__fill_turnons(with_fits)
+        self.__fill_turnons()
+        if with_fits:
+            self.__fit_turnons()
 
         # Overlay the "all" pile-up bin for each threshold
         all_pileup_effs = self.turnons.get_bin_contents([bn.Base.everything])
         hists = []
         labels = []
-        for key in all_pileup_effs.iter_all():
-            if not isinstance(key, int):
+        fits = []
+        for threshold in all_pileup_effs.iter_all():
+            if not isinstance(threshold, int):
                 continue
-            hists.append(all_pileup_effs.get_bin_contents(key))
-            labels.append("> " + str(self.thresholds.bins[key]))
-        self.__make_overlay("all", "all", hists, labels, self.online_title)
+            hists.append(all_pileup_effs.get_bin_contents(threshold))
+            labels.append("> " + str(self.thresholds.bins[threshold]))
+            if with_fits:
+                fits.append(self.fits.get_bin_contents([bn.Base.everything, threshold]))
+        self.__make_overlay("all", "all", hists, fits, labels, self.online_title)
 
         # Overlay individual pile-up bins for each threshold
         for threshold in self.thresholds:
             hists = []
             labels = []
+            fits = []
             for pileup in self.pileup_bins.iter_all():
                 if not isinstance(pileup, int):
                     continue
                 hists.append(self.turnons.get_bin_contents([pileup, threshold]))
+                if with_fits:
+                    fits.append(self.fits.get_bin_contents([pileup, threshold]))
                 labels.append(str(self.pileup_bins.bins[pileup]))
-            self.__make_overlay(pileup, threshold, hists, labels, "PU bin")
+            self.__make_overlay(pileup, threshold, hists, fits, labels, "PU bin")
 
         # Produce the fit summary plot
         if with_fits:
             self.__summarize_fits()
 
-    def __fill_turnons(self, with_fits):
+    def __fill_turnons(self):
         # Boiler plate to convert a given distribution to a turnon
         def make_eff(labels):
             pileup_bin = labels["pileup"]
@@ -92,19 +101,33 @@ class EfficiencyPlot():
             passed = self.yields.get_bin_contents([pileup_bin, threshold_bin])
             turnon = passed.Clone(passed.name.replace("yield", "turnon"))
             turnon.Divide(total)
-            if with_fits:
-                self.__fit_one_turnon(pileup_bin, threshold_bin, turnon)
             return turnon
 
         # Actually make the turnons
         self.turnons = HistogramCollection([self.pileup_bins, self.thresholds],
                                            make_eff)
 
-    def __make_overlay(self, pileup, threshold, hists, labels, header):
+    def __fit_turnons(self):
+        def make_fit(labels):
+            pileup_bin = labels["pileup"]
+            threshold_bin = labels["threshold"]
+            turnon = self.turnons.get_bin_contents([pileup_bin, threshold_bin])
+            params = fit_turnon(turnon, self.thresholds.get_bin_center(threshold_bin))
+            return params
+
+        # Actually make the turnons
+        self.fits = HistogramCollection([self.pileup_bins, self.thresholds],
+                                        make_fit)
+
+    def __make_overlay(self, pileup, threshold, hists, fits, labels, header):
         with preserve_current_style():
             # Draw each turnon (with fit)
             canvas = draw(hists, draw_args={"xtitle": self.offline_title,
                                             "ytitle": "Efficiency"})
+            if len(fits) > 0:
+                for fit, hist in zip(fits, hists):
+                    fit["asymmetric"].linecolor = hist.GetLineColor()
+                    fit["asymmetric"].Draw("same")
 
             # Add labels
             label_canvas()
@@ -125,7 +148,4 @@ class EfficiencyPlot():
             canvas.SaveAs(filename)
 
     def __summarize_fits(self):
-        pass
-
-    def __fit_one_turnon(self, pileup_bin, threshold_bin, turnon):
         pass
