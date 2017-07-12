0ba13a645362297b5501fb4a7baf7703aa8816b1
Ben Krikler <b.krikler@cern.ch>
2017-07-12 13:38:20 +0200

    Tidy up functions for io

--- cmsl1t/io/__init__.py
+++ cmsl1t/io/__init__.py
@@ -1,11 +1,11 @@
+from rootpy.io.pickler import dump, load
+# no pickles without dill
+import dill
 
 def to_root(obj, output_file):
     '''
         Saves the obj into a ROOT file
     '''
-    from rootpy.io.pickler import dump
-    # no pickles without dill
-    import dill
     if not output_file.endswith('.root'):
         output_file += '.root'
     dump(obj, output_file)
@@ -15,7 +15,4 @@ def from_root(input_file):
     '''
         Loads the obj from a ROOT file
     '''
-    from rootpy.io.pickler import load
-    # no pickles without dill
-    import dill
     return load(input_file, use_proxy=False)
