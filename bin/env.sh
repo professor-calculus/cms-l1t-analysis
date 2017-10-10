6070ed8256cc10764be02dc234c7a290ba12fc7f
Alexander Titterton <alextitterton@physics.org>
2017-10-10 17:05:43 +0200

    test branch for the cvms issues

--- bin/env.sh
+++ bin/env.sh
@@ -44,7 +44,7 @@ then
   # this gives you voms-proxy-*, xrdcp and other grid tools
   source /cvmfs/grid.cern.ch/etc/profile.d/setup-cvmfs-ui.sh
   # ROOT 6, voms-proxy-init and other things
-  source /cvmfs/sft.cern.ch/lcg/views/LCG_latest/x86_64-slc6-gcc62-opt/setup.sh
+  source /cvmfs/sft.cern.ch/lcg/views/LCG_90/x86_64-slc6-gcc62-opt/setup.sh
   # to fix java for the hadoop commands:
   unset JAVA_HOME
   pip install --user -r requirements.txt
@@ -80,10 +80,5 @@ PYTHONPATH="${USER_SITE_PACKAGES}:$PYTHONPATH"
 
 git submodule init
 git submodule update
-
-presdir=`pwd`
-cd /afs/cern.ch/work/a/atittert/private/CMSSW/CMSSW_9_2_8/src/
-eval `scramv1 runtime -sh`
-cd $presdir
 
 echo "Environment for ${PROJECT_NAME} is ready"
