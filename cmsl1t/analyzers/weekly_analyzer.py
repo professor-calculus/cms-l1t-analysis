21ab17b8e10f43225c265977171b69c12c4c403b
Luke Kreczko <lkreczko@googlemail.com>
2017-10-27 14:05:46 +0100

    undo changes in weekly_analyzer

--- cmsl1t/analyzers/weekly_analyzer.py
+++ cmsl1t/analyzers/weekly_analyzer.py
@@ -1,10 +1,9 @@
 """
-Make plots for weekly checks
+Make plots for the weekly checks
 """
 
 from BaseAnalyzer import BaseAnalyzer
 from cmsl1t.plotting.efficiency import EfficiencyPlot
-from cmsl1t.collections import EfficiencyCollection
 from cmsl1t.plotting.onlineVsOffline import OnlineVsOffline
 from cmsl1t.plotting.resolution import ResolutionPlot
 from cmsl1t.plotting.resolution_vs_X import ResolutionVsXPlot
@@ -13,101 +12,34 @@ from cmsl1t.playground.eventreader import Met, Sum
 from math import pi
 import pprint
 from collections import namedtuple
-import numpy as np
 
 
-sum_types = [
-    "HTT", "HTT_Emu", "MET_HF", "MET", "MET_PF", "MET_PF_NoMu", "MET_PF_HF",
-    "MET_PF_NoMu_HF", "MET_HF_Emu", "MET_Emu", "MET_PF_Emu", "MET_PF_NoMu_Emu",
-    "MET_PF_HF_Emu", "MET_PF_NoMu_HF_Emu",
-]
-jet_types = [
-    "jetET_B", "jetET_E", "jetET_BE", "jetET_HF",
-    "jetET_B_Emu", "jetET_E_Emu", "jetET_BE_Emu", "jetET_HF_Emu",
-]
+sum_types = ["HTT", "MHT", "MET_HF", "MET_noHF"]
 Sums = namedtuple("Sums", sum_types)
-
-# Eta ranges so we can put |\eta| < val as the legend header on the
-# efficiency plots.
-ETA_RANGES = dict(
-    HTT="|\\eta| < 2.5",
-    MHT="|\\eta| < 2.5",
-    MET_HF="|\\eta| < 5.0",
-    MET="|\\eta| < 3.0",
-    MET_PF="|\\eta| < 3.0",
-    MET_PF_NoMu="|\\eta| < 3.0",
-    MET_PF_HF="|\\eta| < 5.0",
-    MET_PF_NoMu_HF="|\\eta| < 5.0",
-    jetET_B="|\\eta| < 1.479",
-    jetET_E="1.479 < |\\eta| < 3.0",
-    jetET_BE="|\\eta| < 3.0",
-    jetET_HF="3.0 < |\\eta| < 5.0",
-)
-
-THRESHOLDS = dict(
-    HTT=[160, 220, 280, 340, 400],
-    MHT=[40, 60, 80, 100, 120],
-    MET=[40, 60, 80, 100, 120],
-    jetET_B=[35, 60, 90, 140, 180],
-)
-
-HIGH_RANGE_MAX = 2000
-
-for i in ['HF', 'PF', 'PF_NoMu', 'PF_HF', 'PF_NoMu_HF']:
-    THRESHOLDS['MET_' + i] = THRESHOLDS['MET']
-for i in ['E', 'BE', 'HF']:
-    THRESHOLDS['jetET_' + i] = THRESHOLDS['jetET_B']
 
 
 def ExtractSums(event):
-    offline = dict(
-        HTT=Sum(event.sums.Ht),
-        MHT=Met(event.sums.mHt, event.sums.mHtPhi),
-        MET_HF=Met(event.sums.caloMet, event.sums.caloMetPhi),
-        MET=Met(event.sums.caloMetBE, event.sums.caloMetPhiBE),
-        MET_PF=Met(event.sums.met, event.sums.metPhi),
-        MET_PF_NoMu=Met(event.sums.pfMetNoMu, event.sums.pfMetNoMuPhi),
-        MET_PF_HF=Met(event.sums.met, event.sums.metPhi),
-        MET_PF_NoMu_HF=Met(event.sums.pfMetNoMu, event.sums.pfMetNoMuPhi),
-        HTT_Emu=Sum(event.sums.Ht),
-        MHT_Emu=Met(event.sums.mHt, event.sums.mHtPhi),
-        MET_HF_Emu=Met(event.sums.caloMet, event.sums.caloMetPhi),
-        MET_Emu=Met(event.sums.caloMetBE, event.sums.caloMetPhiBE),
-        MET_PF_Emu=Met(event.sums.met, event.sums.metPhi),
-        MET_PF_NoMu_Emu=Met(event.sums.pfMetNoMu, event.sums.pfMetNoMuPhi),
-        MET_PF_HF_Emu=Met(event.sums.met, event.sums.metPhi),
-        MET_PF_NoMu_HF_Emu=Met(event.sums.pfMetNoMu, event.sums.pfMetNoMuPhi)
-    )
-    online = dict(
-        HTT=event.l1Sums["L1Htt"],
-        MHT=event.l1Sums["L1Mht"],
-        MET_HF=event.l1Sums["L1MetHF"],
-        MET=event.l1Sums["L1Met"],
-        MET_PF=event.l1Sums["L1Met"],
-        MET_PF_NoMu=event.l1Sums["L1Met"],
-        MET_PF_HF=event.l1Sums["L1MetHF"],
-        MET_PF_NoMu_HF=event.l1Sums["L1MetHF"],
-        HTT_Emu=event.l1Sums["L1EmuHtt"],
-        MHT_Emu=event.l1Sums["L1EmuMht"],
-        MET_HF_Emu=event.l1Sums["L1EmuMetHF"],
-        MET_Emu=event.l1Sums["L1EmuMet"],
-        MET_PF_Emu=event.l1Sums["L1EmuMet"],
-        MET_PF_NoMu_Emu=event.l1Sums["L1EmuMet"],
-        MET_PF_HF_Emu=event.l1Sums["L1EmuMetHF"],
-        MET_PF_NoMu_HF_Emu=event.l1Sums["L1EmuMetHF"]
-    )
+    offline = dict(HTT=Sum(event.sums.Ht),
+                   MHT=Met(event.sums.mHt, event.sums.mHtPhi),
+                   MET_HF=Met(event.sums.caloMet, event.sums.caloMetPhi),
+                   MET_noHF=Met(event.sums.caloMetBE, event.sums.caloMetPhiBE)
+                   )
+    online = dict(HTT=event.l1Sums["L1Htt"],
+                  MHT=event.l1Sums["L1Mht"],
+                  MET_HF=event.l1Sums["L1MetHF"],
+                  MET_noHF=event.l1Sums["L1Met"]
+                  )
     return online, offline
 
 
 class Analyzer(BaseAnalyzer):
-
     def __init__(self, config, **kwargs):
-        super(Analyzer, self).__init__("weekly_analyzer", config)
+        super(Analyzer, self).__init__("study_met", config)
 
         for name in sum_types:
-            eff_plot = EfficiencyPlot("L1", "offline_" + name)
-            res_plot = ResolutionPlot("energy", "L1", "offline_" + name)
-            twoD_plot = OnlineVsOffline("L1", "offline_" + name)
+            eff_plot = EfficiencyPlot("online" + name, "offline" + name)
+            res_plot = ResolutionPlot("energy", "online" + name, "offline" + name)
+            twoD_plot = OnlineVsOffline("online" + name, "offline" + name)
             self.register_plotter(eff_plot)
             self.register_plotter(res_plot)
             self.register_plotter(twoD_plot)
@@ -115,132 +47,37 @@ class Analyzer(BaseAnalyzer):
             setattr(self, name + "_res", res_plot)
             setattr(self, name + "_2D", twoD_plot)
 
-            eff_plot_HR = EfficiencyPlot("L1", "offline_" + name + "_HiRange")
-            twoD_plot_HR = OnlineVsOffline(
-                "L1", "offline_" + name + "_HiRange")
-            self.register_plotter(eff_plot_HR)
-            self.register_plotter(twoD_plot_HR)
-            setattr(self, name + "_eff_HR", eff_plot_HR)
-            setattr(self, name + "_2D_HR", twoD_plot_HR)
-
-        for angle in sum_types[2:]:
+        for angle in sum_types[1:]:
             name = angle + "_phi"
-            res_plot = ResolutionPlot("phi", "L1", "offline_" + name)
-            twoD_plot = OnlineVsOffline("L1", "offline_" + name)
+            res_plot = ResolutionPlot("phi", "online" + name, "offline" + name)
+            twoD_plot = OnlineVsOffline("online" + name, "offline" + name)
             self.register_plotter(res_plot)
             self.register_plotter(twoD_plot)
             setattr(self, name + "_res", res_plot)
             setattr(self, name + "_2D", twoD_plot)
 
-        for name in jet_types:
-            eff_plot = EfficiencyPlot("L1", "offline_" + name)
-            res_plot = ResolutionPlot("energy", "L1", "offline_" + name)
-            twoD_plot = OnlineVsOffline("L1", "offline_" + name)
-            self.register_plotter(eff_plot)
-            self.register_plotter(res_plot)
-            self.register_plotter(twoD_plot)
-            setattr(self, name + "_eff", eff_plot)
-            setattr(self, name + "_res", res_plot)
-            setattr(self, name + "_2D", twoD_plot)
-
-            eff_plot_HR = EfficiencyPlot("L1", "offline_" + name + "_HiRange")
-            twoD_plot_HR = OnlineVsOffline(
-                "L1", "offline_" + name + "_HiRange")
-            self.register_plotter(eff_plot_HR)
-            self.register_plotter(twoD_plot_HR)
-            setattr(self, name + "_eff_HR", eff_plot_HR)
-            setattr(self, name + "_2D_HR", twoD_plot_HR)
-
-        self.res_vs_eta_CentralJets = ResolutionVsXPlot(
-            "energy", "onlineJet", "offlineJet", "offlineJet_eta")
+        self.res_vs_eta_CentralJets = ResolutionVsXPlot("energy", "onlineJet", "offlineJet", "offlineJet_eta")
         self.register_plotter(self.res_vs_eta_CentralJets)
 
     def prepare_for_events(self, reader):
-        puBins = self.puBins
-        puBins_HR = [0, 999]
+        # TODO: Get these from a common place, and / or the config file
+        puBins = range(0, 50, 10) + [999]
+        thresholds = [70, 90, 110, 130]
 
-        tmpbins0 = range(0, 100, 5)
-        tmpbins1 = range(100, 400, 10)
-        tmpbins2 = range(400, 800, 50)
-        tmpbins3 = range(800, 1000, 200)
-        tmpbins4 = range(1000, 2100, 500)
-        tmpbins0.extend(tmpbins1)
-        tmpbins0.extend(tmpbins2)
-        tmpbins0.extend(tmpbins3)
-        tmpbins0.extend(tmpbins4)
-        # The 'd' arg is vital here, else it will claim axis length zero...
-        xbins = np.asarray(tmpbins0, 'd')
-
-        Config = namedtuple(
-            "Config",
-            "name off_title on_title min max",
-        )
-        cfgs = [
-            Config("HTT", "Offline HTT", "L1 HTT", 30, 600),
-            Config("MHT", "Offline MHT", "L1 MHT", 0, 400),
-            Config("MET_HF", "Offline MET with HF", "L1 MET", 0, 400),
-            Config("MET", "Offline MET no HF", "L1 MET", 0, 400),
-            Config("MET_PF", "Offline PF MET", "L1 MET", 0, 400),
-            Config("MET_PF_NoMu", "Offline PF MET without Muons",
-                   "L1 MET", 0, 400),
-            Config(
-                "MET_PF_HF", "Offline PF MET with HF", "L1 MET", 0, 400,
-            ),
-            Config(
-                "MET_PF_NoMu_HF", "Offline PF MET with HF without Muons",
-                "L1 MET", 0, 400,
-            ),
-
-        ]
-        self._plots_from_cfgs(cfgs)
-        self._plots_from_cfgs(cfgs, emulator=True)
-        self._plots_from_cfgs(cfgs, high_range=True)
-        self._plots_from_cfgs(cfgs, emulator=True, high_range=True)
-
-        self.res_vs_eta_CentralJets.build(
-            "Online Jet energy (GeV)",
-            "Offline Jet energy (GeV)",
-            "Offline Jet Eta (rad)",
-            puBins,
-            50, -0.5, 3.5, 50, -5.0, 5.0,
-        )
-        return True
-
-    def _plots_from_cfgs(self, cfgs, emulator=False, high_range=False):
-        suffix = ""
-        prefix = ""
-        if high_range:
-            suffix = '_HR'
-        if emulator:
-            prefix = '_Emu'
+        Config = namedtuple("Config", "name off_title on_title off_max on_max")
+        HTT_cfg = Config("HTT", "Offline HTT", "Online HTT", 600, 600)
+        MHT_cfg = Config("MHT", "Offline MHT", "Online MHT", 600, 600)
+        MET_HF_cfg = Config("MET_HF", "Offline MET with HF", "Calo MET with HF", 600, 600)
+        MET_noHF_cfg = Config("MET_noHF", "Offline MET no HF", "Calo MET no HF", 600, 600)
+        cfgs = [HTT_cfg, MHT_cfg, MET_HF_cfg, MET_noHF_cfg]
         for cfg in cfgs:
-            eff_plot = getattr(self, cfg.name + prefix + "_eff" + suffix)
-            res_plot = getattr(self, cfg.name + prefix + "_res" + suffix)
-            twoD_plot = getattr(self, cfg.name + prefix + "_2D" + suffix)
-            cfg_max = cfg.max
-            if high_range:
-                cfg_max = HIGH_RANGE_MAX
-
-            eff_plot.build(
-                cfg.on_title,
-                cfg.off_title + " (GeV)",
-                puBins,
-                THRESHOLDS.get(cfg.name),
-                50,
-                cfg.min,
-                cfg_max,
-                legend_title=ETA_RANGES.get(cfg.name, ""))
-            twoD_plot.build(
-                cfg.on_title,
-                cfg.off_title + " (GeV)",
-                puBins,
-                50,
-                cfg.min,
-                cfg_max,
-            )
-
-            if high_range:
-                continue
+            eff_plot = getattr(self, cfg.name + "_eff")
+            res_plot = getattr(self, cfg.name + "_res")
+            twoD_plot = getattr(self, cfg.name + "_2D")
+            eff_plot.build(cfg.on_title + " (GeV)", cfg.off_title + " (GeV)",
+                           puBins, thresholds, 50, 0, cfg.off_max)
+            twoD_plot.build(cfg.on_title + " (GeV)", cfg.off_title + " (GeV)",
+                            puBins, 50, 0, cfg.off_max)
             res_plot.build(cfg.on_title, cfg.off_title,
                            puBins, 50, -10, 10)
 
@@ -248,21 +85,15 @@ class Analyzer(BaseAnalyzer):
                 continue
             res_plot = getattr(self, cfg.name + "_phi_res")
             twoD_plot = getattr(self, cfg.name + "_phi_2D")
-            twoD_plot.build(
-                cfg.on_title + " Phi (rad)",
-                cfg.off_title + " Phi (rad)",
-                puBins, 50,
-                -pi,
-                2 * pi,
-            )
-            res_plot.build(
-                cfg.on_title + " Phi",
-                cfg.off_title + " Phi",
-                puBins,
-                50,
-                -2,
-                2,
-            )
+            twoD_plot.build(cfg.on_title + " Phi (rad)", cfg.off_title + " Phi (rad)",
+                            puBins, 50, -pi, 2 * pi)
+            res_plot.build(cfg.on_title + " Phi", cfg.off_title + " Phi",
+                           puBins, 50, -2, 2)
+
+        self.res_vs_eta_CentralJets.build("Online Jet energy (GeV)",
+                                          "Offline Jet energy (GeV)", "Offline Jet Eta (rad)",
+                                          puBins, 50, -10, 10, 30, 0, 6)
+        return True
 
     def fill_histograms(self, entry, event):
         if not event.passesMETFilter():
@@ -276,53 +107,17 @@ class Analyzer(BaseAnalyzer):
             if on.et == 0:
                 continue
             off = offline[name]
-            for suffix in ['_eff', '_res', '_2D', '_eff_HR', '_2D_HR']:
-                getattr(self, name + suffix).fill(pileup, off.et, on.et)
+            getattr(self, name + "_eff").fill(pileup, off.et, on.et)
+            getattr(self, name + "_res").fill(pileup, off.et, on.et)
+            getattr(self, name + "_2D").fill(pileup, off.et, on.et)
             if hasattr(self, name + "_phi_res"):
                 getattr(self, name + "_phi_res").fill(pileup, off.phi, on.phi)
                 getattr(self, name + "_phi_2D").fill(pileup, off.phi, on.phi)
 
-        goodJets = event.goodJets()
-
         for recoJet in event.goodJets():
-            l1Jet = event.getMatchedL1Jet(recoJet, l1Type='EMU')
+            l1Jet = event.getMatchedL1Jet(recoJet)
             if not l1Jet:
                 continue
-            if recoJet.etCorr > 30.:
-                self.res_vs_eta_CentralJets.fill(
-                    pileup, recoJet.eta, recoJet.etCorr, l1Jet.et)
-
-        leadingRecoJet = event.getLeadingRecoJet()
-        if not leadingRecoJet:
-            return True
-
-        l1EmuJet = event.getMatchedL1Jet(leadingRecoJet, l1Type='EMU')
-        if not l1EmuJet:
-            return True
-
-        fillRegions = []
-        if abs(leadingRecoJet.eta) < 1.479:
-            fillRegions = ['B', 'BE']
-        elif abs(leadingRecoJet.eta) < 3.0:
-            fillRegions = ['E', 'BE']
-        else:
-            fillRegions = ['HF']
-        for region in [fillRegions:
-            for suffix in ['_eff', '_res', '_2D', '_eff_HR', '_2D_HR']:
-                name = 'jetET_{0}_Emu{1}'.format(region, suffix)
-                getattr(self, name).fill(
-                    pileup, leadingRecoJet.etCorr, l1EmuJet.et,
-                )
-
-        l1Jet = event.getMatchedL1Jet(leadingRecoJet, l1Type='HW')
-        if not l1Jet:
-            return True
-
-        for region in [fillRegions:
-            for suffix in ['_eff', '_res', '_2D', '_eff_HR', '_2D_HR']:
-                name = 'jetET_{0}{1}'.format(region, suffix)
-                getattr(self, name).fill(
-                    pileup, leadingRecoJet.etCorr, l1Jet.et,
-                )
+            self.res_vs_eta_CentralJets.fill(pileup, recoJet.eta, recoJet.etCorr, l1Jet.et)
 
         return True
